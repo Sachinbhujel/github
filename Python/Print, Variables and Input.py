2b84df927b668858650cbe3c74473a1b387eb93f
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-25 14:34:48 +0000

    print learn

--- Python/Print, Variables and Input.py
+++ Python/Print, Variables and Input.py
@@ -1,6 +1,32 @@
 print("Hello world!")
 
-# variable 
-name = "sachin"
-age = 17
+# Variables:- is used to store values and variables store data in a computer memory.
+# Variables can't start with number.
+# Special characters and hyphen are not allowed when naming a variable.
+name = "sachin"  # string 
+age = 17  # integer
+isHero = True  # boolean
+class_marks = 76.3  # float 
 print(name)
+print(class_marks)
+
+# Overwrite the values of variable
+class_marks = 87
+print(class_marks)
+
+# Multiple variable
+firstName, lastName, age, isMarried = "sachin", "bhujel", 16 , True
+print(firstName, lastName)
+print(firstName * age)
+print(class_marks, age)
+print(class_marks + age)
+
+# Input:- Taking information from the user.
+name = input("what is your name?")
+print("hello " + name)
+
+
+
+
+
+
