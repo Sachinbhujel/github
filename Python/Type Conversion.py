fa1023da1552653b121f042e1567bf3509fa84d0
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-26 04:46:42 +0000

    ok

---
+++ Python/Type Conversion.py
@@ -0,0 +1,24 @@
+# Type casting or Type conversion:- Converting one data type to another data type.
+
+# Types of conversion
+# 1. str()
+# 2. int()
+# 3. bool()
+# 4. float()
+# 5. list()
+
+# str to int
+oldAge = input("Enter your age? ")
+newAge = int(oldAge) + 2
+print(newAge)
+
+# float to int
+gravity = 9.77
+print(int(gravity))
+
+# str to list
+word = "hello"
+print(word)
+wordTolist = list(word)
+print(list(word))
+print(wordTolist)
