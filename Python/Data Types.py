6344cc0a6bd8ef66bee5c185a9b13f0b7e1296b4
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-26 04:26:00 +0000

    ok

---
+++ Python/Data Types.py
@@ -0,0 +1,14 @@
+firstName = "Sachin"  # str
+age = 17  # int
+is_married = False  # boolean
+floatNum = 12.3  # float
+myList = ["sachin", 12, "sajan", 18]  # list
+myDict = {"maths": 54, "hindi":76, "science": 32}  # dictionary
+
+# Checking data types
+print(type(firstName))
+print(type(age))
+print(type(is_married))
+print(type(floatNum))
+print(type(myList))
+print(type(myDict))
