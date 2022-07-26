6ae9edbbf670a9bf5164174e7627c559bdf89075
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-26 05:19:50 +0000

    ok

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,32 @@
+# Addition(+)
+print(5 + 7)
+
+# Subtraction(-)
+print(87 - 11)
+
+# Multiplication(*)
+print(34 * 2)
+
+# Division(/)
+print(5 / 2)
+
+# Floor division
+print(5 // 2)
+
+# Modules
+print(5 % 2)
+
+# Exponential or Power(**)
+print(5 ** 2)
+print(5 ** 3)
+
+# Shortcuts for arithmetic operations
+num = 5
+num = num + 3
+num += 3 # same as num = num + 3
+num -= 2 # same as num = num - 2
+#num /= 5 # same as num =  num / 5
+#num *= 5 # same as num = num * 5
+num //= 2 # same as num =  num // 2
+num **= 2 # same as num = num ** 2
+print(num)
