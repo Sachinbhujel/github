cfe0ac5b0f03b353c30b5ff8e976b993cdb024b7
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-28 03:27:26 +0000

    okk

---
+++ Python/Comparison Operators.py
@@ -0,0 +1,23 @@
+# Greater than
+print(3 > 2)
+
+# Less than
+print(3 < 2)
+
+# Greater than or equal to
+print(3 >= 2)
+
+# Less than or equal to 
+print(3 <= 2)
+
+# Equal
+print(3 == 3)
+
+# Not Equal(kya ye dono alag hai)
+print(3 != 13)
+
+# Comparing something gives either a True or False
+print("True == True:",True == True)
+print("True == False:",True == False)
+print("False == False:",False == False)
+
