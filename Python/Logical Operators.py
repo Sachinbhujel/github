05796960714885bf44ec686dc05b9761f81303e7
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-07-28 03:37:29 +0000

    hiiiii

---
+++ Python/Logical Operators.py
@@ -0,0 +1,10 @@
+# or operator: Returns True if One Of the statement is true 
+print(3 > 13 or 2 < 3)
+
+# and operator: Returs True if both the statements are true
+print(2 > 3 and 2 > 1)
+
+# not operator: Reverse the results returns False if the results is true
+print(not 2 > 3)
+print(not True)
+print(not not True)
